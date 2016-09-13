ab749c04712976cb4da731b2acafdb2c14e8a961
Fredrik Jonsén <fredrikjonsen@gmail.com>
2016-09-13 16:33:33 +0200

    Made service servers and database

---
+++ services/persons/personsServer.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('database.db');
+
+var app = express();
+
+app.get('/list', function(req, res) {
+  var stmt = db.all("SELECT * FROM person;", function(err, rows) {
+    res.send(rows);
+  });
+})
+
+app.get('/find.name', function(req, res) {
+  var name = req.query.name;
+  if (name) {
+    db.get("SELECT * FROM person WHERE name=(?) COLLATE NOCASE", {1: name}, function(err, row) {
+      if (row) res.send({key: row.id, name: row.name});
+      else res.send('null');
+    });
+  } else {
+    res.send('null');
+  }
+});
+
+app.get("/find.key", function(req, res) {
+  var id = req.query.key;
+  if (id) {
+    db.get("SELECT * FROM person WHERE id=(?)", {1: id}, function(err, row) {
+      console.log(row);
+      if (row) res.send({key: row.id, name: row.name});
+      else res.send('null');
+    });
+  }
+  else {
+    res.send('null');
+  }
+})
+
+db.serialize(function() {
+  app.listen(8060);
+})
