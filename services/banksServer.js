311a629a04c2215b5b335ae23b8aeb72e67aeffe
Fredrik Jonsén <fredrikjonsen@gmail.com>
2016-10-20 18:55:09 +0200

    Fixed db issue

--- services/banksServer.js
+++ services/banksServer.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('database.db');
+var db = new sqlite3.Database(':memory:');
 
 var app = express();
 
@@ -44,5 +44,29 @@ app.get("/find.key", function(req, res) {
 })
 
 db.serialize(function() {
+  var content = [
+    ["7fe7b9a7b3a9168cfbd1a2af2c58aaa6", "Swedbank"],
+    ["6cecd13127d3b6f6cb1e5838f98b45cf", "Handelsbanken"],
+    ["d5f440167865dd4ff59bf4203e60d131", "SEB"],
+    ["2cc223ccf22194b36219b6223311831a", "Min Spargris"],
+    ["3cb936b8e4b3adc13f64bc0bf4b96d04", "SWEDBANK"],
+    ["2cc223ccf22194b36219b6223311831a", "IKANOBANKEN"],
+    ["38fe3dd0ec4092dd11cd9b8878344cba", "JPMORGAN"],
+    ["226656f6f6674dec466c41638c09cffb", "NORDEA"],
+    ["2bffb0f1bc8aec1e985e8a42415bcec6", "CITIBANK"],
+    ["e659099fc8c554c7ee9c3dbd5c94425d", "HANDELSBANKEN"],
+    ["e659099fc8c554c7ee9c3dbd5c94425d", "SBAB"],
+    ["e659099fc8c554c7ee9c3dbd5c94425d", "HSBC"],
+    ["4e87b6057e549ac5d55e84cf38e7ca3c", "NORDNET"]
+  ];
+
+  db.run("CREATE TABLE bank(id varchar(32), name varchar(30))");
+
+  var stmt = db.prepare("INSERT INTO bank VALUES (?, ?)");
+  for (var i = 0; i < content.length; i++) {
+    stmt.run(content[i][0], content[i][1]);
+  }
+  stmt.finalize();
+
   app.listen(8070);
 })
--- services/personsServer.js
+++ services/personsServer.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('database.db');
+var db = new sqlite3.Database(':memory:');
 
 var app = express();
 
@@ -41,5 +41,26 @@ app.get("/find.key", function(req, res) {
 });
 
 db.serialize(function() {
+
+  var content = [
+    ["1e8a4f8a29989789cbb6726f14934f2f", "Fredrik"],
+    ["a9680b66edc99c248b6ad6020d485d16", "Fredric"],
+    ["d73f0d6fd1dfaa8c86010fd8afb48b9d", "Eric"],
+    ["77f76600dc6b02d307710dd4e7a0e61b", "Emelie"],
+    ["f8478ebb3665fa15ae8f69a3f9e752b0", "Jakob Pogulis"],
+    ["aa6724ccce7d470d5d96ecaa241fbf7e", "Xena"],
+    ["e4f1435c3d891642c86d643a0601ca92", "Marcus Bendtsen"],
+    ["eadd47fa78b68c0529d739b2b810c69a", "Zorro"],
+    ["7b041153eb1909788e2e5d8486fdc01b", "Q"]
+  ];
+
+  db.run("CREATE TABLE person(id varchar(32), name varchar(30))");
+
+  var stmt = db.prepare("INSERT INTO person VALUES (?, ?)");
+  for (var i = 0; i < content.length; i++) {
+    stmt.run(content[i][0], content[i][1]);
+  }
+  stmt.finalize();
+
   app.listen(8060);
 });
